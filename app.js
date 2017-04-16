f9e8db1b3bdfb77c36e9653f84c92f582ba6efa5
TIM SAELEE <timsaelee@TIMs-MacBook-Pro.local>
2017-04-16 03:49:33 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+var restify = require('restify');
+var builder = require('botbuilder');
+
+//=========================================================
+// Bot Setup
+//=========================================================
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url); 
+});
+  
+// Create chat bot
+var connector = new builder.ChatConnector({
+    appId: process.env.81917d54-a4c3-47ef-818e-6420d3573420,
+    appPassword: process.env.avFdnBGwR1jDW6Cfv2HSkor
+});
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
+
+//=========================================================
+// Bots Dialogs
+//=========================================================
+
+bot.dialog('/', function (session) {
+    session.send("Hello World");
+});
